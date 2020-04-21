10e1026e0c00c34a6ba40c4951af1379b81e38e6
oNguyenVanHuy <nguyen.van.huy@sun-astersik.com>
2020-04-22 01:35:31 +0700

    First Django App Part 2

--- django_project/mysite/polls/models.py
+++ django_project/mysite/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+  text = models.CharField(max_length = 200)
+  publish_date = models.DateTimeField('Date published')
+
+  def __str__(self):
+    return self.text
+
+class Choice(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choise_text = models.CharField(max_length = 200)
+  votes = models.IntegerField(default = 0)
+
+  def __str__(self):
+    return self.choise_text
